43d6cb1519a9f52ab4a4af91a87e7c3618411e51
kmh1996 <99661776+kmh1996@users.noreply.github.com>
2022-02-15 11:20:31 +0900

    Delete 10_access_modifier directory

--- 10_access_modifier/src/a1/A.java
+++
@@ -1,5 +0,0 @@
-package a1;
-
-public class A {
-	B b = new B();
-}
--- 10_access_modifier/src/a3/B.java
+++
@@ -1,7 +0,0 @@
-package a3;
-
-public class B {
-	A a = new A(); 		// public
-	A a1 = new A(1);	// default
-	//A a2 = new A("이기적인놈");	// private
-}
